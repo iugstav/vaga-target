cafb7b6a373f2b382b7ce31e35b1e705cffe4003
Gustavo Soares <soaresgu2710@gmail.com>
2024-09-04 16:25:41 -0300

    fix: added output to questão_2.py and removed a random file

--- main.py
+++
@@ -1,9 +0,0 @@
-index = 12
-soma = 0
-k = 1
-
-while k < index:
-    k += 1
-    soma = soma + k
-
-print(soma)
--- questao_2.py
+++ questao_2.py
@@ -1,5 +1,8 @@
-def tem_letra_a(string):
+def conta_letra_a(string):
     ocurrences = 0
     for c in string:
         if c == "a" or c == "A":
             ocurrences += 1
+
+text = input("informe o seu texto: ")
+print(f"seu texto possui {conta_letra_a(text)} ocorrências da letra 'a', sendo ela maiúscula ou minúscula")
